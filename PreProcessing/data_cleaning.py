b2196012fb47cc6c7f15170aae8858b815706310
Espoir Badohoun <espoir.badohoun@gmail.com>
2024-01-05 10:33:38 +0100

    Ajout du deuxième  élément de notre checklist de projet datascience à savoir la phase de preprocessing

---
+++ PreProcessing/data_cleaning.py
@@ -0,0 +1,117 @@
+""""
+
+
+
+Objectif: transformer le data pour le mettre dans un format propice au machine learning
+
+
+
+	§ Création du Train Set / Test Set : split des données 
+
+	§ Élimination des NaN : dropna(), imputation, colonne"vides"
+
+	§ Encodage
+
+	§ Suppression des outliers néfastes au modèle
+
+	§ Feature selection
+
+	§ Feature engineering
+
+	§ Feature scaling
+
+
+"""
+
+
+
+import logging
+from abc import ABC, abstractmethod
+from typing import Union
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+class DataStrategy(ABC):
+    """
+    Classe abstraite définissant notre stratégie de traitement des données
+    """
+
+    @abstractmethod
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        pass
+
+
+class DataPreprocessStrategy(DataStrategy):
+    """
+    Stratégie de prétraitement des données qui prétraite les données.
+    """
+
+    def handle_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Supprime les colonnes qui ne sont pas obligatoires, remplit les valeurs manquantes avec des valeurs moyennes médianes et convertit le type de données en flottant.
+        """
+        try:
+            data = data.drop(
+                [
+                    "order_approved_at",
+                    "order_delivered_carrier_date",
+                    "order_delivered_customer_date",
+                    "order_estimated_delivery_date",
+                    "order_purchase_timestamp",
+                ],
+                axis=1,
+            )
+            data["product_weight_g"].fillna(data["product_weight_g"].median(), inplace=True)
+            data["product_length_cm"].fillna(data["product_length_cm"].median(), inplace=True)
+            data["product_height_cm"].fillna(data["product_height_cm"].median(), inplace=True)
+            data["product_width_cm"].fillna(data["product_width_cm"].median(), inplace=True)
+            # write "No review" in review_comment_message column
+            data["review_comment_message"].fillna("No review", inplace=True)
+
+            data = data.select_dtypes(include=[np.number])
+            cols_to_drop = ["customer_zip_code_prefix", "order_item_id"]
+            data = data.drop(cols_to_drop, axis=1)
+
+            return data
+        except Exception as e:
+            logging.error(e)
+            raise e
+
+
+class DataDivideStrategy(DataStrategy):
+    """
+    Stratégie de division des données qui divise les données en données d'entraînement et de test.
+    """
+
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Divides the data into train and test data.
+        """
+        try:
+            X = data.drop("review_score", axis=1)
+            y = data["review_score"]
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=0.2, random_state=42
+            )
+            return X_train, X_test, y_train, y_test
+        except Exception as e:
+            logging.error(e)
+            raise e
+
+
+class DataCleaning:
+    """
+    Classe de nettoyage des données qui prétraite les données et les divise en données d'entraînement et de test.
+    """
+
+    def __init__(self, data: pd.DataFrame, strategy: DataStrategy) -> None:
+        """Initializes the DataCleaning class with a specific strategy."""
+        self.df = data
+        self.strategy = strategy
+
+    def handle_data(self) -> Union[pd.DataFrame, pd.Series]:
+        """Handle data based on the provided strategy"""
+        return self.strategy.handle_data(self.df)
