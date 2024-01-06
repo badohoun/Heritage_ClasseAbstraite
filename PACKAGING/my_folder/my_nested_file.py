81e1debc558eca2de5014854ac84ce9eb1936388
Espoir Badohoun <espoir.badohoun@gmail.com>
2024-01-06 16:05:47 +0100

    add first hack : add __init__.py in the root  not recommanded to fix our import statement issues : import relative from hierarchy

--- PACKAGING/my_folder/my_nested_file.py
+++ PACKAGING/my_folder/my_nested_file.py
@@ -1,4 +1,4 @@
-from ..my_other_file import CONSTANT as CONSTANT2 # not use this relative import 
+from PACKAGING.my_other_file import CONSTANT as CONSTANT2 # not use this relative import 
 # in this case we are trying to import something from a file higher in the file hierarchy into the file more deeply in the file hierarchy
 
 CONSTANT = "hello"
