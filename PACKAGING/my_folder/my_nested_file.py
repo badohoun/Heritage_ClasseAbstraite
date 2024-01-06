9dc02d88baf2e49f7bdb0edc0592b66839c9b17d
Espoir Badohoun <espoir.badohoun@gmail.com>
2024-01-06 16:12:44 +0100

    second hack to solve import file statetements issues : use module sys to insert a path or module in a pythonpath environnement variable notrecommanded

--- PACKAGING/my_folder/my_nested_file.py
+++ PACKAGING/my_folder/my_nested_file.py
@@ -1,3 +1,6 @@
+import sys
+print(sys.path)
+sys.path.insert(0,"/Users/espoirbadohoun/trainingclassabstraite/Heritage_ClasseAbstraite")
 from PACKAGING.my_other_file import CONSTANT as CONSTANT2 # not use this relative import 
 # in this case we are trying to import something from a file higher in the file hierarchy into the file more deeply in the file hierarchy
 
